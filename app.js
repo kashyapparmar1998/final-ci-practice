f56080026263199235703da9a8818f3557cf52da
kashyapparmar1998 <kashyapparmar1998@gmail.com>
2026-08-20 12:48:49 +0530

    Initial commit with working unit test

---
+++ app.js
@@ -0,0 +1,1 @@
+function checkEven(num) { return num % 2 === 0; } module.exports = checkEven;
---
+++ test.js
@@ -0,0 +1,1 @@
+const checkEven = require('./app'); if (checkEven(4) !== true) { throw new Error('Failed'); } console.log('Test Passed!');
